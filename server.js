2daa647660fb2360b102e9e1f55ea5c6daf67ee5
Shambhavi Kuthe <kutheshambhavi1@gmail.com>
2022-10-04 11:42:35 -0400

    added line chart

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var bodyparser = require("body-parser");
 var mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://adityagawali:adityagawali@cluster0.qhmboym.mongodb.net/ASDatabase?retryWrites=true&w=majority");
-
+mongoose.connect("mongodb+srv://userSK:DatabasePass19@cluster0.hhjwnou.mongodb.net/ASDatabase?retryWrites=true&w=majority");
+// mongodb+srv://adityagawali:adityagawali@cluster0.qhmboym.mongodb.net/ASDatabase?retryWrites=true&w=majority
 
 const ejs = require("ejs");
 //const { default: mongoose } = require("mongoose");
@@ -18,7 +18,14 @@ var schema = new mongoose.Schema({
     country: String
 })
 
+var schemaPage2 = new mongoose.Schema({
+    month: String,
+    data: Number,
+})
+
 var detailsModel = mongoose.model("as",schema);
+var page2DetailsModel = mongoose.model("as_details",schemaPage2);
+
 
 app.get("/",function(req,res)
 {
@@ -41,7 +48,6 @@ app.get('/:number', function(req, res){
     console.log(asNumber.number);
     res.render("pages/detailNumber",{
         number: asNumber.number
-
     });
 
   });
